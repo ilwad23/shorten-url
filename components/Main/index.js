107be0a86f8eaabaca57c8be22bd0427b960f503
ilwad <ilwadabukor@hotmail.com>
2022-09-12 20:43:17 +0100

    update

--- components/Main/index.js
+++ components/Main/index.js
@@ -6,13 +6,13 @@ export default function Main() {
   return (
     <main className="main">
       <Shorten />
-      {/* <div className="hook">
+      <div className="hook">
         <h2 className="hook__title">Advanced Statistics</h2>
         <p className="hook__para">
           Track how your links are performing across the web with our advanced
           statistics dashboard.
         </p>
-      </div> */}
+      </div>
       <Benefits />
       <div className="CTA">
         <h2 className="CTA__tilte">Boost your links today</h2>
