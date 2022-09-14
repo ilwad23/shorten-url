a17cdd179a46a257c032b2ca3bb3cda18cfd9ab8
ilwad <ilwadabukor@hotmail.com>
2022-09-14 09:22:31 +0100

    update

--- components/Main/Shorten/Form.js
+++ components/Main/Shorten/Form.js
@@ -1,27 +1,36 @@
 import React from "react";
 
-export default function Form({ oriAddress, setOriAddress }) {
+export default function Form({
+  oriAddress,
+  setOriAddress,
+  error,
+  errorMessage,
+  validateInput,
+  submitURL,
+}) {
   return (
-    <div className="form">
+    <form className="form" onSubmit={(e) => submitURL(e)}>
       <div className="form__container">
         <div className="form__input">
           <input
+            title="Enter url"
             value={oriAddress}
+            onFocus={(e) => validateInput(e,true)}
             onChange={(e) => setOriAddress(e.target.value)}
             className={`form__inputField ${
-              true ? "form__inputField--error" : ""
+              error ? "form__inputField--error" : ""
             }`}
             type="url"
             placeholder="Shorten a link here..."
           />
-          <p className={`form__para ${true ? "form__para--error" : ""}`}>
-            please add a link
+          <p className={`form__para ${error ? "form__para--error" : ""}`}>
+            {errorMessage}
           </p>
         </div>
-        <button className="form__btn btn">
+        <button className="form__btn btn" onClick={(e) => submitURL(e)}>
           <p>Shorten it</p>
         </button>
       </div>
-    </div>
+    </form>
   );
 }
--- components/Main/Shorten/index.js
+++ components/Main/Shorten/index.js
@@ -1,15 +1,46 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import Form from "./form";
 export default function Shorten() {
-  const [oriAddress, setOriAddress] = useState('https://www.frontendmentor.io')
-  const [newAddress, setNewAddress] = useState('https://rel.ink/k4lKyk')
-  const [addresses, setAddresses] = useState([{addresses:{origin:oriAddress, new:newAddress}}])
- console.log(oriAddress);
+  // const [oriAddress, setOriAddress] = useState('https://www.frontendmentor.io')
+  const [errorMessage, setErrorMessage] = useState("please add a link");
+  const [error, setError] = useState(false);
+  const [oriAddress, setOriAddress] = useState("");
+  const [newAddress, setNewAddress] = useState("https://rel.ink/k4lKyk");
+  const [addresses, setAddresses] = useState([
+    { addresses: { origin: oriAddress, new: newAddress } },
+  ]);
+  // console.log(error,addresses);
+  const validateInput = (e, focus = false) => {
+    let regex = /(www|http:|https:)+[^\s]+[\w]/;
+    if (focus) {
+      setError(false);
+    } else if (oriAddress === "") {
+      setErrorMessage("please add a link");
+      setError(true);
+    } else if (!regex.test(oriAddress)) {
+      setErrorMessage("please add a valid link");
+      setError(true);
+    }
+  };
+  const submitURL = (e) => {
+    e.preventDefault();
+    validateInput(e);
+    setAddresses;
+    console.log(errorMessage);
+  };
   return (
     <div className="shortenURL">
-      <Form oriAddress={oriAddress} setOriAddress={setOriAddress}/>
+      <Form
+        oriAddress={oriAddress}
+        setOriAddress={setOriAddress}
+        error={error}
+        setError={setError}
+        errorMessage={errorMessage}
+        validateInput={validateInput}
+        submitURL={submitURL}
+      />
       <div className="list">
-        {addresses.map(({addresses},i) => (
+        {addresses.map(({ addresses }, i) => (
           <div className="list__item" key={"item" + i}>
             <p className="list__oriAddress">{addresses.origin}</p>
             <p className="list__newAddress">{addresses.new}</p>
